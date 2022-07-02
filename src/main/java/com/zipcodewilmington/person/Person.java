c111f313bed925991e7817dd5f06467eaf71765e
Tenae <tenae@zipcodersmbp107.myfiosgateway.com>
2022-07-02 19:22:17 -0400

    completed lab

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -7,29 +7,98 @@ public class Person {
     private String name;
     private int age;
 
+    private String phoneNumber;
+    private String ZwcCohort;
+    private boolean scholarship;
+    private String background;
+    private int lockerNumber;
+
+
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
+
     }
 
-    public Person(String name, int age) {
+    public Person(String name, int age, String phoneNumber, String ZwcCohort, boolean scholarship, String background, int lockerNumber) {
+        this.name = name; // this.name from field = name from parameter.
+        this.age= age;
+        this.phoneNumber = phoneNumber; // this.phoneNumber from field you created = phoneNumber from parameter you just created.
+        this.ZwcCohort = ZwcCohort;
+        this.scholarship = scholarship;
+        this.background = background;
+        this.lockerNumber = lockerNumber;
+
     }
 
     public void setName(String name) {
+        this.name = name;
+
     }
 
     public void setAge(int age) {
+        this.age = age;
+
+    }
+    public  void setPhoneNumber (String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+
+    }
+
+    public void setZwcCohort (String ZwcCohort){
+        this.ZwcCohort = ZwcCohort;
+    }
+    public void setScholarship (Boolean scholarship){
+        this.scholarship = scholarship;
+    }
+
+    public void setBackground (String background){
+        this.background = background;
+    }
+
+    public void setLockerNumber (Integer LockerNumber){
+        this.lockerNumber = LockerNumber;
     }
 
     public String getName() {
-        return null;
+
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+
+        return this.age;
     }
-}
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public String getZwcCohort (){
+        return this.ZwcCohort;
+    }
+    public Boolean getScholarship (){
+        return this.scholarship;
+    }
+
+    public String getBackground ( ){
+        return this.background;
+    }
+
+    public  Integer getLockerNumber ( ){
+        return this.lockerNumber;
+    }
+
+
+
+
+    }
+
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -56,16 +56,31 @@ public class TestPerson {
         // Given
         Integer expectedAge = 5;
         String expectedName = "Leon";
+       String expectedPhoneNumber = "3025652864";
+        String expectedZwcCohort = "Java 8.1";
+        boolean expectedScholarship = false;
+        String expectedBackground = "Research";
+        int expectedLockerNumber = 66;
 
         // When
-        Person person = new Person(expectedName, expectedAge);
+        Person person = new Person(expectedName, expectedAge, expectedPhoneNumber, expectedZwcCohort, expectedScholarship, expectedBackground, expectedLockerNumber);
 
         // Then
         Integer actualAge = person.getAge();
         String actualName = person.getName();
+        String actualPhoneNumber = person.getPhoneNumber();
+        String actualZcwCohort = person.getZwcCohort();
+        boolean actualScholarship = person.getScholarship();
+        String actualBackground = person.getBackground();
+        int actualLockerNumber = person.getLockerNumber();
 
         Assert.assertEquals(expectedAge, actualAge);
         Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedZwcCohort, actualZcwCohort);
+        Assert.assertEquals(expectedPhoneNumber, actualPhoneNumber);
+        Assert.assertEquals(expectedBackground, actualBackground);
+        Assert.assertEquals(expectedScholarship, actualScholarship);
+        Assert.assertEquals(expectedLockerNumber, actualLockerNumber);
     }
 
     @Test
@@ -83,6 +98,62 @@ public class TestPerson {
     }
 
     @Test
+    public void testSetZcwCohort() {
+        // Given
+        Person person = new Person();
+        String expected = "Java 8.1";
+
+        // When
+        person.setZwcCohort(expected);
+
+        // Then
+       String actual = person.getZwcCohort();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetPhoneNumber() {
+        // Given
+        Person person = new Person();
+        String expected = "4871837362";
+
+        // When
+        person.setPhoneNumber(expected);
+
+        // Then
+        String actual = person.getPhoneNumber();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetBackground() {
+        // Given
+        Person person = new Person();
+        String expected = "research";
+
+        // When
+        person.setBackground(expected);
+
+        // Then
+        String actual = person.getBackground();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetScholarship() {
+        // Given
+        Person person = new Person();
+        Boolean expected = false;
+
+        // When
+        person.setScholarship(expected);
+
+        // Then
+        Boolean actual = person.getScholarship();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
     public void testSetAge() {
         // Given
         Person person = new Person();
@@ -93,6 +164,20 @@ public class TestPerson {
 
         // Then
         Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetLockerNumber() {
+        // Given
+        Person person = new Person();
+        Integer expected = 5;
+
+        // When
+        person.setLockerNumber(expected);
+
+        // Then
+        Integer actual = person.getLockerNumber();
         Assert.assertEquals(expected, actual);
     }
 }
